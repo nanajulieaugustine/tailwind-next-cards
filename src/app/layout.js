3c6287cd6ceea1fb9a9e2e5234b0ab42bdd24fe0
Dannie Vinther <davi@kea.dk>
2025-03-31 07:23:19 +0200

    Enhance layout styling with full-screen background gradient

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen grid place-content-center bg-linear-to-r/oklab from-pink-300 to-indigo-400`}
       >
         {children}
       </body>
